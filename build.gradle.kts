2fa546ef92c6e4bd8c5a6c7a5d40297e60ab48bf
david <mrminecraft00@gmail.com>
2024-03-04 13:32:47 +0100

    incremented version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "net.thenextlvl.msg"
-version = "1.0.2"
+version = "1.0.3"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_19
--- src/main/java/net/thenextlvl/msg/velocity/MessagePlugin.java
+++ src/main/java/net/thenextlvl/msg/velocity/MessagePlugin.java
@@ -32,7 +32,7 @@ import java.util.WeakHashMap;
 @Plugin(
         id = "vanilla-msg",
         name = "VanillaMSG",
-        version = "1.0.2",
+        version = "1.0.3",
         url = "https://thenextlvl.net",
         description = "A simple msg plugin using brigadier",
         authors = "NonSwag"
