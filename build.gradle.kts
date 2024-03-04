b34b044e02c00518c309fd6aa1031db253aba3a8
david <mrminecraft00@gmail.com>
2024-03-04 07:44:45 +0100

    updated java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ group = "net.thenextlvl.msg"
 version = "1.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
 }
 
 repositories {
